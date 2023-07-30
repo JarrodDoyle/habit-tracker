6fb445722305c4257fe69228df1de08e0c9abf46
Jarrod Doyle <thejarroddoyle@gmail.com>
2023-07-30 10:22:52 +0100

    Extracted sidebar page buttons to a component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import SidebarButton from "./SidebarButton.vue";
 </script>
 
 <template>
@@ -6,27 +7,10 @@
     <div class="font-display text-xl px-1 py-2 self-center"><span class="text-emerald-500">Habit</span> Tracker</div>
     <hr class="border-slate-900">
 
-    <!-- TODO: Extract to a component -->
-    <button
-      class="bg-slate-800 rounded flex flex-row text-white p-2 space-x-2 items-center hover:bg-slate-900 active:bg-slate-700">
-      <img src="icons/today-outline-20-white.svg" alt="Today Icon" />
-      <span class="font-body text-xs">Today</span>
-    </button>
-
-    <button class="rounded flex flex-row text-white p-2 space-x-2 items-center hover:bg-slate-900 active:bg-slate-700">
-      <img src="icons/social-leaderboard-outline-20-white.svg" alt="Today Icon" />
-      <span class="font-body text-xs">Habits</span>
-    </button>
-
-    <button class="rounded flex flex-row text-white p-2 space-x-2 items-center hover:bg-slate-900 active:bg-slate-700">
-      <img src="icons/interests-outline-20-white.svg" alt="Today Icon" />
-      <span class="font-body text-xs">Categories</span>
-    </button>
-
-    <button class="rounded flex flex-row text-white p-2 space-x-2 items-center hover:bg-slate-900 active:bg-slate-700">
-      <img src="icons/settings-outline-20-white.svg" alt="Today Icon" />
-      <span class="font-body text-xs">Settings</span>
-    </button>
+    <SidebarButton icon="icons/today-outline-20-white.svg" text="Today" selected />
+    <SidebarButton icon="icons/social-leaderboard-outline-20-white.svg" text="Habits" />
+    <SidebarButton icon="icons/interests-outline-20-white.svg" text="Categories" />
+    <SidebarButton icon="icons/settings-outline-20-white.svg" text="Settings" />
 
     <hr class="border-slate-900">
 
---
+++ src/components/SidebarButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+    text?: string,
+    icon?: string,
+    selected?: boolean,
+}>();
+</script>
+
+<template>
+    <button :class="{ 'bg-slate-800': selected }"
+        class="rounded flex flex-row text-white p-2 space-x-2 items-center hover:bg-slate-900 active:bg-slate-700">
+        <img v-if="icon" :src="icon" alt="Icon" />
+        <span v-if="text" class="font-body text-xs">{{ text }}</span>
+    </button>
+</template>
